1b0319808b1f6260822ad89b594abd2da873a1ee
Dent Dentoo <Dentoo@users.noreply.github.com>
2014-11-20 17:28:13 +0100

    Update tvrage_poster_sub.pl

--- tvrage_poster_sub.pl
+++ tvrage_poster_sub.pl
@@ -14,24 +14,21 @@ print "You must enter an url beginning with http. \n";
 exit;
 }
 
-# Set the url value to the argument passed from commandline
-my $url=$ARGV[0];
-
 # Subroutine for tvrage poster url
 sub tvrageposter {
-   my $imageid="not found";
-   my $content = get $url;
-   if ($content =~ /src='http:\/\/images.tvrage.com\/shows\/(.*?)'><\/div>/){
-   my $imageid = ${1};
-   $imageid =~ s/^/http:\/\/images.tvrage.com\/shows\//g;
-   print "TVRage poster image URL: $imageid \n";
-   }elsif ($imageid eq "not found"){
-   print "No Imageid found \n";
-   }
+my $imageid="not found";
+my $content = get $_[0];
+if ($content =~ /src='http:\/\/images.tvrage.com\/shows\/(.*?)'><\/div>/){
+my $imageid = ${1};
+$imageid =~ s/^/http:\/\/images.tvrage.com\/shows\//g;
+print "TVRage poster image URL: $imageid \n";
+}elsif ($imageid eq "not found"){
+print "No Imageid found \n";
+}
 }
 
 # Use function with the argument passed to it
-print tvrageposter;
+print tvrageposter($ARGV[0]);
 
 # Exit
 exit;
